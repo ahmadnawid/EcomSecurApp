559a6921f1fce6884b293fa973b03d0bd742697f
Ahmad Nawid <ahmadnawid.mz@gmail.com>
2024-12-19 10:54:07 +0100

    spring security app created

---
+++ src/main/java/com/app/EcomSecurApp/EcomSecurAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.EcomSecurApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomSecurAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomSecurAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/EcomSecurApp/EcomSecurAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.EcomSecurApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomSecurAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
