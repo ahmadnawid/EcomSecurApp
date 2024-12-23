f43f2b07a9d5a6a8b912f459cb8e9d020d3a1f0f
Ahmad Nawid <ahmadnawid.mz@gmail.com>
2024-12-23 11:39:53 +0100

    spring security, csrf token config

---
+++ src/main/java/com/app/EcomSecurApp/StudentController.java
@@ -0,0 +1,39 @@
+package com.app.EcomSecurApp;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Ahmad", 70),
+            new Student(2, "Nawid", 90),
+            new Student(3, "John", 60),
+            new Student(4, "Joker", 55)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+            return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
+
+}
