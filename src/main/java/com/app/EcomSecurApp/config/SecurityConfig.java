c9abaa0f1784a5f31bdbe4345919d955f183b8cd
Ahmad Nawid <ahmadnawid.mz@gmail.com>
2024-12-31 10:42:30 +0100

    validate jwt token

--- src/main/java/com/app/EcomSecurApp/config/SecurityConfig.java
+++ src/main/java/com/app/EcomSecurApp/config/SecurityConfig.java
@@ -20,10 +20,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Autowired
     private MyUserDetailService userDetailService;
@@ -38,7 +42,8 @@ public class SecurityConfig {
                     .anyRequest().authenticated())
         //http.formLogin(Customizer.withDefaults());
             .httpBasic(Customizer.withDefaults())
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
